c578541b0f844b041f4daa550b6c2bb4a7b275e3
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2023-08-25 23:55:07 +0530

    Update math_test.go

--- Unit-testing/math/math_test.go
+++ Unit-testing/math/math_test.go
@@ -9,13 +9,13 @@ type AddData struct {
 
 func TestAdd(t *testing.T) {
 
-	// result := Add(1, 3)
+	result := Add(1, 3)
 
-	// if result != 4 {
-	// 	t.Errorf("Add(1, 3) FAILED. Expected %d, got %d\n", 4, result)
-	// } else {
-	// 	t.Logf("Add(1, 3) PASSED. Expected %d, got %d\n", 4, result)
-	// }
+	if result != 4 {
+		t.Errorf("Add(1, 3) FAILED. Expected %d, got %d\n", 4, result)
+	} else {
+		t.Logf("Add(1, 3) PASSED. Expected %d, got %d\n", 4, result)
+	}
 	testdemo := []AddData{
 		{1, 3, 4},
 		{-1, 6, 5},
